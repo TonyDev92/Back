90b729642b5be93270b638e794f0f5ad0f805215
TonyDev92 <tonydeveloper92@gmail.com>
2023-05-05 22:07:19 +0200

    middleware Auths reload

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -37,20 +37,20 @@ const isProtectoraEntity = async (req, res , next) => {
 
     try {
         const authorization = req.headers.authorization;
-        if(!authorization){
+        if(!authorization){ //TEST AUTHORIZATION 
             return res.status(401).json({message: 'unauthorized'})
         }
         const token = authorization.split(' ')[1];
-        if(!token){
+        if(!token){ //TEST TOKEN
             return res.status(401).json({message : 'no token provided'});
         }
         let tokenVerified = verifySign(token , process.env.JWT_KEY);
-        if(!tokenVerified.id){
+        if(!tokenVerified.id){ //TEST TOKEN VERIFIED
             return res.status(401).json(tokenVerified);
         }
         const userLogged = await User.findById(tokenVerified.id);
         req.user = userLogged;
-        if(userLogged.role !== 'protectora'){
+        if(userLogged.role !== 'protectora'){ //TEST ROLE 
             return res.status(401).json({message : 'need to be protectora'})
         }
         next();
