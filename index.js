11b6d849b18860657d2a4ecb4cff00c005a5c09b
TonyDev92 <tonydeveloper92@gmail.com>
2023-05-05 16:15:56 +0200

    userModel

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const { connect } = require('./src/utils/database');
+const router = require('./src/api/routes/routes');
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
+
+app.use('/user', routerUser);
+
 
 connect();
 
---
+++ src/api/models/usermodel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        email: {type: String , required: true},
+        password: {type : String , required: true},
+        pets:[{ type:String , required: false}],
+        role: {type: String , default: 'user', enum:'user'}
+    },{
+        timestamps:true
+    }
+)
+
+const User = mongoose.model('user', userSchema);
+module.exports = User;
