acd0c92240cb42a6fdebae1a06585e7f2475df9e
TonyDev92 <tonydeveloper92@gmail.com>
2023-05-08 12:01:01 +0200

    UserModelUpdate

--- src/api/models/usermodel.js
+++ src/api/models/usermodel.js
@@ -4,10 +4,12 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
     {
         email: {type: String , required: true},
+        name: {type : String , required : true},
+        surname: {type: String , required : true},
         password: {type : String , required: true},
-        pets:[{ type:String , required: false}], //Array para almacenar las mascotas de Usuario
-        favorites: [{type:String , required: false}], //Almacenamos Favoritos
-        adoptionStatus: [{type:String, required:false}],
+        pets:[{ type: mongoose.Types.ObjectId, ref: 'Animales' , required: false}], //Array para almacenar las mascotas de Usuario
+        favorites: [{type: mongoose.Types.ObjectId, ref: 'Animales' , required: false}], //Almacenamos Favoritos
+        adoptionStatus: [{type: mongoose.Types.ObjectId, ref: 'protectora' , required: false}],
         role: {type: String , default: 'user', enum:'user'}
     },{
         timestamps:true
